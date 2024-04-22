de3170467b4c7f5d6670b7de7f4c9c7b3d35a9f0
Lloyd-Badeji <adetomisoye@gmail.com>
2024-04-22 07:45:49 +0100

    loginsinup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Banner from "./assets/components/Banner.jsx"
 import Footer from "./assets/components/Footer.jsx"
 import Nav from "./assets/components/Nav.jsx"
+import SignUp from "./assets/components/loginsignup/Signup.jsx"
+import Login from "./assets/components/loginsignup/Login.jsx"
 
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
       <Nav />
       <Banner />
       <Footer />
+      <SignUp />
+      <Login />
      
       </div>
     </>
---
+++ src/assets/components/loginsignup/Login.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    onLogin({ username, password });      
+  };
+
+  return (
+    <>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;

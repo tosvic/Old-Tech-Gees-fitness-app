40822c34e500ec3485268ab65acade418911f23b
Lloyd-Badeji <152262810+Lloyd-Badeji@users.noreply.github.com>
2024-04-23 16:24:08 +0100

    New loginsignup

--- src/components/loginsignup/Login.jsx
+++ src/components/loginsignup/Login.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "../loginsignup/login.css"
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    onLogin({ username, password });      
+  };
 
   const navigate = useNavigate
   return (
     <div>
-
       <h1>Welcome to Login</h1>
-
+      <div className='border'>
+        <h2 id='Head'>Login</h2>
+       <form onSubmit={handleLogin} className='User'>
+         <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+         />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" id='submit'>Login</button>
+        </form>
+      </div>
       <div>
        <a onClick={()=>navigate('/MainLayout')} href="Login">Login</a>
       </div>
--- src/components/loginsignup/Signup.jsx
+++ src/components/loginsignup/Signup.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../loginsignup/signup.css'
 
-const Signup = () => {
+const Signup = ({ onSignUp }) => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    onSignUp({ username, password });
+  };
+
   return (
     <div>
-
       <h1>Welcome</h1>
-
       <div>
-        <p>Already have an acccount <a onClick={()=>navigate('./Login')} href="Login">login</a></p>
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSignUp}>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" id='submit'>Sign Up</button>
+          <div>
+            <p>Already have an acccount <button><a onClick={() => navigate('./Login')} href="Login">login</a></button></p>
+          </div>
+        </form>
       </div>
+
 
     </div>
   )
